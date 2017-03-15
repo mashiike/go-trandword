52bcd3a451121df0635cac5504396b21a9fcbfac
mashiike <ikeda-masashi@kayac.com>
2017-03-15 22:52:11 +0900

    update main

--- main.go
+++ main.go
@@ -2,34 +2,18 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
 	"github.com/ikawaha/kagome/tokenizer"
 )
 
 func main() {
-	var input = flag.String("input", "data/default.txt", "input file")
-	var modef = flag.Bool("mode-file", false, "input mode file read flag")
-	flag.Parse()
-
-	var reader io.Reader
-
-	if *modef {
-		var err error
-		reader, err = os.Open(*input)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		reader = os.Stdin
-	}
 
 	t := tokenizer.New()
-	scanner := bufio.NewScanner(reader)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	vcab := make(map[string]int)
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
 			panic(err)
@@ -37,13 +21,18 @@ func main() {
 		tokens := t.Tokenize(scanner.Text())
 		for _, token := range tokens {
 			if token.Class == tokenizer.DUMMY {
-				// BOS: Begin Of Sentence, EOS: End Of Sentence.
-				fmt.Printf("%s\n", token.Surface)
 				continue
 			}
-			features := strings.Join(token.Features(), ",")
-			fmt.Printf("%s\t%v\n", token.Surface, features)
+			features := token.Features()
+			if features[0] == "名詞" {
+				vcab[token.Surface]++
+			}
 		}
+	}
+
+	fmt.Println("word,frequency")
+	for k, v := range vcab {
+		fmt.Printf("\"%s\",\"%d\"\n", k, v)
 	}
 
 }
