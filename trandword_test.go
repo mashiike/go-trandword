060b11a6271b60d9fbb068043bd9e1fd9cff758b
mashiike <ikeda-masashi@kayac.com>
2017-03-27 02:04:06 +0900

    change test package name

--- trandword_test.go
+++ trandword_test.go
@@ -1,14 +1,15 @@
-package trandword
+package trandword_test
 
 import (
 	"bytes"
 	"testing"
+
+	"github.com/mashiike/go-trandword"
 )
 
 func TestNewTrandword(t *testing.T) {
 	reader := bytes.NewBufferString("foo\n")
-	if tw := NewTrandword(reader); tw == nil {
+	if tw := trandword.NewTrandword(reader); tw == nil {
 		t.Fatalf("NewTrandword() : nil expected not nil")
 	}
-
 }
